048cf9a96f42ff20eba671d6ac3c92ab6916dddd
章宛枫David Zhang <david_tianhao_zhang@foxmail.com>
2023-01-06 03:55:54 -0500

    Soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID NEW_EVENT = 3050945240U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PITCH
+        {
+            static const AkUniqueID GROUP = 1908158473U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID A = 84696446U;
+                static const AkUniqueID A_SHARP = 164139463U;
+                static const AkUniqueID B = 84696445U;
+                static const AkUniqueID C = 84696444U;
+                static const AkUniqueID C_SHARP = 2816864521U;
+                static const AkUniqueID D = 84696443U;
+                static const AkUniqueID D_SHARP = 844677726U;
+                static const AkUniqueID E = 84696442U;
+                static const AkUniqueID F = 84696441U;
+                static const AkUniqueID F_SHARP = 951594152U;
+                static const AkUniqueID G = 84696440U;
+                static const AkUniqueID G_SHARP = 3736223277U;
+            } // namespace SWITCH
+        } // namespace PITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PITCH = 1908158473U;
+        static const AkUniqueID PLAYBACK_SPEED = 78830142U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
